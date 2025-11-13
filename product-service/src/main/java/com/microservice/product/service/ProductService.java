2630aa5ad8521291cf0af982390c6218c5902de6
Thivya15 <thivyamahendran15@gmail.com>
2025-11-13 14:21:41 +0530

    Implemented Product Service with MongoDB integration using Docker container

---
+++ product-service/src/main/java/com/microservice/product/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.microservice.product.service;
+
+import com.microservice.product.dto.ProductRequest;
+import com.microservice.product.dto.ProductResponse;
+import com.microservice.product.model.Product;
+import com.microservice.product.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created successfully");
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
